5c238a427093acd8fd5090ae0f121728d6cf1e60
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-08-04 11:09:18 +0700

    feat: тестирование burger-constructor

--- src/services/actions/feedWebSocket.ts
+++ src/services/actions/feedWebSocket.ts
@@ -1,8 +1,8 @@
-import { createAction } from "@reduxjs/toolkit";
-import { IFetchOrdersData } from "./../../utils/types";
+import { createAction } from '@reduxjs/toolkit';
+import { IFetchOrdersData } from './../../utils/types';
 
 export type Data = {
-  type: "DATA";
+  type: 'DATA';
   data: IFetchOrdersData;
 };
 
@@ -10,14 +10,14 @@ export type TWSAction = Data;
 
 export type TWSActions = Array<TWSAction>;
 
-export const connect = createAction<string, "WS_CONNECT">("WS_CONNECT");
-export const disconnect = createAction("WS_DISCONNECT");
-export const wsConnecting = createAction("WS_CONNECTING");
-export const wsOpen = createAction("WS_OPEN");
-export const wsClose = createAction("WS_CLOSE");
-export const wsMessage = createAction<TWSActions, "WS_MESSAGE">("WS_MESSAGE");
-export const wsError = createAction<string, "WS_ERROR">("WS_ERROR");
-export const countConnect = createAction<number>("COUNT_CONNECT");
+export const connect = createAction<string, 'WS_CONNECT'>('WS_CONNECT');
+export const disconnect = createAction('WS_DISCONNECT');
+export const wsConnecting = createAction('WS_CONNECTING');
+export const wsOpen = createAction('WS_OPEN');
+export const wsClose = createAction('WS_CLOSE');
+export const wsMessage = createAction<TWSActions, 'WS_MESSAGE'>('WS_MESSAGE');
+export const wsError = createAction<string, 'WS_ERROR'>('WS_ERROR');
+export const countConnect = createAction<number>('COUNT_CONNECT');
 
 export const wsActions = {
   wsConnect: connect,
