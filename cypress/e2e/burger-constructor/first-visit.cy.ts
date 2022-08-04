0cfbe67203d89b7e913fa9642eba333ed911246a
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-08-04 11:52:14 +0700

    feat: remove cypress

--- cypress/e2e/burger-constructor/first-visit.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('При первом визите кнопка оформить заказ неактивна', function () {
-  before(function() {
-    cy.visit('http://localhost:3000');
-  })
-  it('Кнопка не активна', function () {
-    cy.get('[class^=button_button__]').first().as('orderButton');
-    cy.get('@orderButton').should('be.disabled');
-  });
-});
